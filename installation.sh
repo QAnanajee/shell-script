1ca467ac96c53ae7ccf8bde39fe5a500a68b46cc
Nanajee ad <nanajee.k9@gmail.com>
2024-04-26 13:00:21 +0530

    commit all files

--- installation.sh
+++ installation.sh
@@ -10,3 +10,23 @@ echo "you are super user"
 fi
 
 dnf install mysql -y
+
+if [ $?-ne 0 ]
+then
+echo "Installtion of mysql....FAILURE"
+exit 1
+else 
+echo "Installtion of mysql....SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $?-ne 0 ]
+then
+echo "Installtion of git....FAILURE"
+exit 1
+else 
+echo "Installtion of git....SUCCESS"
+fi
+
+echo "Is script recording?"
