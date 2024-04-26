98f561304a8780bca8241deb145a7472dc1f2299
Nanajee ad <nanajee.k9@gmail.com>
2024-04-26 11:47:24 +0530

    commit all files

--- conditions1.sh
+++ conditions1.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if[ $NUMBER -gt 10 ]
+if [ $NUMBER -gt 10 ]
 then
-    echo "Given number is greaterthan 10"
+    echo "Given number $NUMBER is greaterthan 10"
 else
-    echo "Giiven number is lessthan 10"
+    echo "Giiven number $NUMBER is lessthan 10"
 fi
