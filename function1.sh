10738077efc47aeadb9ae904eedae47be46e806a
Nanajee ad <nanajee.k9@gmail.com>
2024-04-26 20:43:10 +0530

    all files to push

---
+++ function1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE() {
+    echo "Exit status: $1"
+    echo "what are you doing: $2"
+}
+
+if [ $USERID -ne 0 ]
+then
+echo "Please run this script with root access"
+exit 1
+else
+echo "you are super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+
+dnf install git -y
+VALIDATE $? "Installing mysql"
+
+echo "Is script recording?"
