411fd1108419899e9d53d2f12e514e20794a56c1
Nanajee ad <nanajee.k9@gmail.com>
2024-04-27 12:41:52 +0530

    add packages intsallation

--- install-packages.sh
+++ install-packages.sh
@@ -6,12 +6,17 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 if [ $USERID -ne 0 ] 
 then
     echo "Please run this script with root access"
     exit 1
 else
-    echo "you are super user"
+    echo -e "$R you are super user $N"
 fi
 
 for i in $@
@@ -21,6 +26,8 @@ do
 
     if [ $? -eq 0 ]
     then
-        echo "$i already installed.....so SKIPPED"
+        echo -e "$Y $i already installed.....so SKIPPED $N"
+    else
+        echo -e "$G $i not installed.....need to install... $N"
     fi
 done
