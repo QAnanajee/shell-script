ee0a5937b2717fcd07170bf89ae5e91d7b771875
Nanajee ad <nanajee.k9@gmail.com>
2024-05-06 09:25:11 +0530

    current to other script files

--- 16-other-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-COURSE="Online course of AWS DEVOPS by SIVA KUMAR REDDY"
-
-echo "Variable from other script, course is: $COURSE"
-
-echo "Process ID of current script: $$"
-
---
+++ other-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COURSE="New course from Siva"
+
+echo "Variable calling other script, course is: $COURSE"
+
+echo "Process ID of current script: $$"
