93af330909659d911708935453e715334029d1f3
Nanajee ad <nanajee.k9@gmail.com>
2024-05-06 08:56:52 +0530

    current to other script files

---
+++ 15-current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COURSE="Learning AWS DEVOPS by SIVA"
+
+echo "Before calling other script, course is: $COURSE"
+
+echo "Process ID of current script: $$"
+
+#./16-other-script.sh
+
+echo "After calling other script, course is: $COURSE"
