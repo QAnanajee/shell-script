7daac1a9863259d716b7a919bf8d6f4c548af682
Nanajee ad <nanajee.k9@gmail.com>
2024-05-12 08:36:04 +0530

    add cto files

---
+++ set.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+echo "Please run this script with root access"
+exit 1
+else
+echo "you are super user"
+fi
+
+dnf install mysql12345 -y
+
+dnf install git -y
+
+echo "Is script recording?"
